6da1f0edd7e5636ffd939e6b5cfcb4c489c11975
Nicollas1305 <cartola1320@gmail.com>
2024-02-28 00:18:06 -0300

    refactor: remove homepage exemplo, cria map clean e referencia Home.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:testing_maps/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Testing maps',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}

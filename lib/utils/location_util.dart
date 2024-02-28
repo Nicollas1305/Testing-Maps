6871ba0e6ab1e52082215d73410fd940661704d8
Nicollas1305 <cartola1320@gmail.com>
2024-02-28 00:20:07 -0300

    feat: cria imagens de mapa estáticas

---
+++ lib/utils/location_util.dart
@@ -0,0 +1,24 @@
+const googleApiKey = 'AIzaSyBvJ4YZ-lW4s5Flr6rwn-sfEHd-0Ad5KiU';
+
+class LocationUtil {
+  static String generateLocationPreviewImage({
+    double? latitude,
+    double? longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=18&size=600x300&maptype=satellite&markers=7Ccolor:red%7C$latitude,$longitude&key=$googleApiKey';
+  }
+
+  static String generateLocationPreviewMapCircleSatellite({
+    double? latitude,
+    double? longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=18&size=600x300&maptype=satellite&key=$googleApiKey';
+  }
+
+  static String generateLocationPreviewMapCircleTerrain({
+    double? latitude,
+    double? longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=18&size=600x300&maptype=roadmap&key=$googleApiKey';
+  }
+}
